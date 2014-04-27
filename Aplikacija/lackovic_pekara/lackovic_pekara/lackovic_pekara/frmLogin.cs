da0883f20ddf34d894fd753af40fcbe942f27804
Marija <malackovi@foi.hr>
2014-04-27 19:45:14 +0200

    Obrana projekta
    dokumentacija i početak aplikacije

---
+++ Aplikacija/lackovic_pekara/lackovic_pekara/lackovic_pekara/frmLogin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lackovic_pekara
+{
+    public partial class frmLogin : Form
+    {
+        private frmMain parent;
+        public frmLogin(frmMain arg)
+        {
+            InitializeComponent();
+            parent = arg;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string lozinka = txtPassword.Text;
+            this.osobaTableAdapter.FillByUsername(pekara_bazaDataSet.osoba, username);
+            try
+            {
+                string ime = this.pekara_bazaDataSet.osoba.Rows[0]["ime"].ToString();
+                string ocekivana_lozinka = this.pekara_bazaDataSet.osoba.Rows[0]["lozinka"].ToString();
+                if (ocekivana_lozinka == lozinka)
+                {
+                    parent.after_login(ime);
+                    this.Close();
+                }
+                else {
+                    txtPassword.Text = "";
+                    lblgreska.Visible = true;
+                    lblgreska.Text = "Pogrešna lozinka";
+                }
+            }
+            catch {
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                lblgreska.Visible = true;
+                lblgreska.Text = "Nepostojeći korisnik";
+                return;
+            }
+           
+        }
+    }
+}
--- Aplikacija/lackovic_pekara/lackovic_pekara/lackovic_pekara/frmMain.Designer.cs
+++ Aplikacija/lackovic_pekara/lackovic_pekara/lackovic_pekara/frmMain.Designer.cs
@@ -28,20 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.datotekaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.zatvoriMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ocisti_timer = new System.Windows.Forms.Timer(this.components);
+            this.zaprimanjeMaterijalaMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.izdavanjeMaterijalaMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.upravljanjeProizvodimaMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            this.status.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.datotekaToolStripMenuItem,
+            this.zaprimanjeMaterijalaMenu,
+            this.izdavanjeMaterijalaMenu,
+            this.upravljanjeProizvodimaMenu});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(684, 24);
+            this.menuStripMain.TabIndex = 1;
+            this.menuStripMain.Text = "menuStripMain";
+            // 
+            // datotekaToolStripMenuItem
+            // 
+            this.datotekaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginMenu,
+            this.logoutMenu,
+            this.zatvoriMenu});
+            this.datotekaToolStripMenuItem.Name = "datotekaToolStripMenuItem";
+            this.datotekaToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.datotekaToolStripMenuItem.Text = "Datoteka";
+            // 
+            // loginMenu
+            // 
+            this.loginMenu.Name = "loginMenu";
+            this.loginMenu.Size = new System.Drawing.Size(152, 22);
+            this.loginMenu.Text = "Login";
+            this.loginMenu.Click += new System.EventHandler(this.loginMenu_Click);
+            // 
+            // logoutMenu
+            // 
+            this.logoutMenu.Name = "logoutMenu";
+            this.logoutMenu.Size = new System.Drawing.Size(152, 22);
+            this.logoutMenu.Text = "Logout";
+            this.logoutMenu.Visible = false;
+            this.logoutMenu.Click += new System.EventHandler(this.logoutMenu_Click);
+            // 
+            // zatvoriMenu
+            // 
+            this.zatvoriMenu.Name = "zatvoriMenu";
+            this.zatvoriMenu.Size = new System.Drawing.Size(152, 22);
+            this.zatvoriMenu.Text = "Zatvori";
+            this.zatvoriMenu.Click += new System.EventHandler(this.zatvoriMenu_Click);
+            // 
+            // status
+            // 
+            this.status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.status.Location = new System.Drawing.Point(0, 440);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(684, 22);
+            this.status.TabIndex = 3;
+            this.status.Text = "statusStrip";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ocisti_timer
+            // 
+            this.ocisti_timer.Interval = 8000;
+            this.ocisti_timer.Tick += new System.EventHandler(this.ocisti_timer_Tick);
+            // 
+            // zaprimanjeMaterijalaMenu
+            // 
+            this.zaprimanjeMaterijalaMenu.Name = "zaprimanjeMaterijalaMenu";
+            this.zaprimanjeMaterijalaMenu.Size = new System.Drawing.Size(134, 20);
+            this.zaprimanjeMaterijalaMenu.Text = "Zaprimanje materijala";
+            this.zaprimanjeMaterijalaMenu.Visible = false;
+            // 
+            // izdavanjeMaterijalaMenu
+            // 
+            this.izdavanjeMaterijalaMenu.Name = "izdavanjeMaterijalaMenu";
+            this.izdavanjeMaterijalaMenu.Size = new System.Drawing.Size(123, 20);
+            this.izdavanjeMaterijalaMenu.Text = "Izdavanje materijala";
+            this.izdavanjeMaterijalaMenu.Visible = false;
+            // 
+            // upravljanjeProizvodimaMenu
+            // 
+            this.upravljanjeProizvodimaMenu.Name = "upravljanjeProizvodimaMenu";
+            this.upravljanjeProizvodimaMenu.Size = new System.Drawing.Size(147, 20);
+            this.upravljanjeProizvodimaMenu.Text = "Upravljanje proizvodima";
+            this.upravljanjeProizvodimaMenu.Visible = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::lackovic_pekara.Properties.Resources.back;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.menuStripMain);
+            this.DoubleBuffered = true;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripMain;
             this.Name = "frmMain";
             this.Text = "Pekara Aplikacija";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.status.ResumeLayout(false);
+            this.status.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem datotekaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginMenu;
+        private System.Windows.Forms.ToolStripMenuItem logoutMenu;
+        private System.Windows.Forms.ToolStripMenuItem zatvoriMenu;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Timer ocisti_timer;
+        private System.Windows.Forms.ToolStripMenuItem zaprimanjeMaterijalaMenu;
+        private System.Windows.Forms.ToolStripMenuItem izdavanjeMaterijalaMenu;
+        private System.Windows.Forms.ToolStripMenuItem upravljanjeProizvodimaMenu;
     }
 }
 
