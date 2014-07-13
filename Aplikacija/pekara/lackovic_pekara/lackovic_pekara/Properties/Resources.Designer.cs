3f8d12ece4e969983ba30fd90f075168b7ae4095
Marija <malackovi@foi.hr>
2014-07-13 16:31:18 +0200

    Gotovo za obranu
    zadnja verzija (zadnje promjene)

--- Aplikacija/pekara/lackovic_pekara/lackovic_pekara/Properties/Resources.Designer.cs
+++ Aplikacija/pekara/lackovic_pekara/lackovic_pekara/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace PI.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap back {
-            get {
-                object obj = ResourceManager.GetObject("back", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
